fa2cb9d1e4dcb3a6a44bd3a5c260907d85304b2a
bossboss123123 <becauseyou0111@gmail.com>
2026-06-28 21:51:03 +0800

    Delete win_repo directory

--- win_repo/uninstall.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo 正在移除 YT Downloader...
-schtasks /end /tn "YTDownloaderServer" >nul 2>&1
-schtasks /delete /tn "YTDownloaderServer" /f >nul 2>&1
-taskkill /f /im YTDownloaderServer.exe >nul 2>&1
-echo ✓ 已移除背景服務
-pause
